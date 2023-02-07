04526ea210fd3783ecf2fd44a53c3ee8d1c41bdc
Juha Nummi <juhawilliam@gmail.com>
2023-02-07 09:19:10 +0200

    1.3 done

---
+++ part1/kurssitiedot/src/App.js
@@ -0,0 +1,59 @@
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.part1.name} {props.part1.exercises}
+      </p>
+      <p>
+        {props.part2.name} {props.part2.exercises}
+      </p>
+      <p>
+        {props.part2.name} {props.part2.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+    </div>
+  )
+}
+
+export default App
