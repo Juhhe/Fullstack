5a0a355887194600bff01419621c602dc5e77443
Juha Nummi <juhawilliam@gmail.com>
2023-05-12 10:54:44 +0300

    Part4 tests done

---
+++ part4/blogilista/utils/list_helper.js
@@ -0,0 +1,42 @@
+const lodash = require('lodash')
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const reducer = (favorite, blog) => {
+        if (favorite.likes > blog.likes) {
+            return favorite
+        }
+        return blog
+    }
+    return blogs.reduce(reducer, {})
+}
+
+const mostBlogs = (blogs) => {
+    const authors = lodash.countBy(blogs, 'author')
+    const author = lodash.maxBy(lodash.keys(authors), i => authors[i])
+    return { author: author, blogs: authors[author] }
+}
+
+const mostLikes = (blogs) => {
+    const authors = lodash.groupBy(blogs, 'author')
+    const author = lodash.maxBy(lodash.keys(authors), i => totalLikes(authors[i]))
+    return { author: author, likes: totalLikes(authors[author]) }
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
+}
